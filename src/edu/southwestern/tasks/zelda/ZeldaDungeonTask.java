4f66efa121b97e152797aecb880e0cb7d78ba841
Jacob Schrum <schrum2@southwestern.edu>
2020-01-03 23:53:50 -0600

    refactored code
    single exit point so that tracking fitness less complicated

--- src/edu/southwestern/tasks/zelda/ZeldaDungeonTask.java
+++ src/edu/southwestern/tasks/zelda/ZeldaDungeonTask.java
@@ -25,6 +25,7 @@ public abstract class ZeldaDungeonTask<T> extends NoisyLonerTask<T> {
 		// Objective functions
 		MMNEAT.registerFitnessFunction("DistanceToTriforce");
 		MMNEAT.registerFitnessFunction("NegativeRooms"); // Fewer rooms means more interesting shapes
+		MMNEAT.registerFitnessFunction("PercentRoomsTraversed"); // Avoid superfluous rooms
 		// Additional information tracked about each dungeon
 		MMNEAT.registerFitnessFunction("NumRooms",false);
 		MMNEAT.registerFitnessFunction("NumRoomsTraversed",false);
@@ -51,69 +52,65 @@ public abstract class ZeldaDungeonTask<T> extends NoisyLonerTask<T> {
 	@Override
 	public Pair<double[], double[]> oneEval(Genotype<T> individual, int num) {
 		Dungeon dungeon = getZeldaDungeonFromGenotype(individual);
-		if(dungeon == null) {
-			// The A* fix could not make the level beat-able. This probably means that the state space was too big to search,
-			// so this level should receive minimal fitness.
-			return new Pair<double[], double[]>(new double[]{-100, 0}, new double[] {0, 0, 0});
-		}
-		int numRooms = dungeon.getLevels().size();
+		int distanceToTriforce = -100; // Very bad fitness if level is not beatable 
+		int numRooms = 0;
 		int searchStatesVisited = 0;
-		// A* should already have been run during creation to assure beat-ability, but it is run again here to get the action sequence.
-		ArrayList<GridAction> actionSequence;
-		HashSet<ZeldaState> mostRecentVisited;
-		try {
-			//			actionSequence = DungeonUtil.makeDungeonPlayable(dungeon);
-			Search<GridAction,ZeldaState> search = new AStarSearch<>(ZeldaLevelUtil.manhattan);
-			ZeldaState state = new ZeldaState(5, 5, 0, dungeon);
+		int numRoomsTraversed = 0;
+		if(dungeon != null) {
 			try {
-				actionSequence = ((AStarSearch<GridAction, ZeldaState>) search).search(state, true, Parameters.parameters.integerParameter("aStarSearchBudget"));
-			}catch(IllegalStateException e) {
-				throw e; // Pass on exception, but the finally assures we save states when things go wrong.
-			} finally {
-				mostRecentVisited = ((AStarSearch<GridAction, ZeldaState>) search).getVisited();
-				searchStatesVisited = mostRecentVisited.size();
-			}
+				numRooms = dungeon.getLevels().size();
+				// A* should already have been run during creation to assure beat-ability, but it is run again here to get the action sequence.
+				ArrayList<GridAction> actionSequence;
+				HashSet<ZeldaState> mostRecentVisited;
+				//actionSequence = DungeonUtil.makeDungeonPlayable(dungeon);
+				Search<GridAction,ZeldaState> search = new AStarSearch<>(ZeldaLevelUtil.manhattan);
+				ZeldaState state = new ZeldaState(5, 5, 0, dungeon);
+				try {
+					actionSequence = ((AStarSearch<GridAction, ZeldaState>) search).search(state, true, Parameters.parameters.integerParameter("aStarSearchBudget"));
+					if(actionSequence != null) 
+						distanceToTriforce = actionSequence.size();
+				}catch(IllegalStateException e) {
+					throw e; // Pass on exception, but the finally assures we save states when things go wrong.
+				} finally {
+					mostRecentVisited = ((AStarSearch<GridAction, ZeldaState>) search).getVisited();
+					searchStatesVisited = mostRecentVisited.size();
+				}
 
+				HashSet<Pair<Integer,Integer>> visitedRoomCoordinates = new HashSet<>();
+				for(ZeldaState zs: mostRecentVisited) {
+					// Set does not allow duplicates: one Pair per room
+					visitedRoomCoordinates.add(new Pair<>(zs.dX,zs.dY));
+				}
 
-		} catch(IllegalStateException e) {
-			// But sometimes this exception occurs anyway. Not sure why, but we can take this to mean the level has a problem and deserves bad fitness
-			return new Pair<double[], double[]>(new double[]{-100, -numRooms}, new double[] {numRooms, 0, searchStatesVisited});
-		}
+				numRoomsTraversed = visitedRoomCoordinates.size();
 
-		int distanceToTriforce = actionSequence == null ? -100 : actionSequence.size();
-
-		HashSet<Pair<Integer,Integer>> visitedRoomCoordinates = new HashSet<>();
-		for(ZeldaState zs: mostRecentVisited) {
-			// Set does not allow duplicates: one Pair per room
-			visitedRoomCoordinates.add(new Pair<>(zs.dX,zs.dY));
-		}
-
-		int numRoomsTraversed = visitedRoomCoordinates.size();
-
-		if(CommonConstants.watch) {
-			System.out.println("Distance to Triforce: "+distanceToTriforce);
-			System.out.println("Number of rooms: "+numRooms);
-			System.out.println("Number of rooms traversed: "+numRoomsTraversed);
-			System.out.println("Number of states visited: "+searchStatesVisited);
-			// View whole dungeon layout
-			DungeonUtil.viewDungeon(dungeon, mostRecentVisited);			
-			System.out.println("Enter 'P' to play, or just press Enter to continue");
-			String input = MiscUtil.waitForReadStringAndEnterKeyPress();
-			System.out.println("Entered \""+input+"\"");
-			if(input.toLowerCase().equals("p")) {
-				new Thread() {
-					@Override
-					public void run() {
-						// Repeat dungeon generation to remove visited marks
-						Dungeon dungeon = getZeldaDungeonFromGenotype(individual);
-						RougelikeApp.startDungeon(dungeon);
+				if(CommonConstants.watch) {
+					System.out.println("Distance to Triforce: "+distanceToTriforce);
+					System.out.println("Number of rooms: "+numRooms);
+					System.out.println("Number of rooms traversed: "+numRoomsTraversed);
+					System.out.println("Number of states visited: "+searchStatesVisited);
+					// View whole dungeon layout
+					DungeonUtil.viewDungeon(dungeon, mostRecentVisited);			
+					System.out.println("Enter 'P' to play, or just press Enter to continue");
+					String input = MiscUtil.waitForReadStringAndEnterKeyPress();
+					System.out.println("Entered \""+input+"\"");
+					if(input.toLowerCase().equals("p")) {
+						new Thread() {
+							@Override
+							public void run() {
+								// Repeat dungeon generation to remove visited marks
+								Dungeon dungeon = getZeldaDungeonFromGenotype(individual);
+								RougelikeApp.startDungeon(dungeon);
+							}
+						}.start();
+						System.out.println("Press enter");
+						MiscUtil.waitForReadStringAndEnterKeyPress();
 					}
-				}.start();
-				System.out.println("Press enter");
-				MiscUtil.waitForReadStringAndEnterKeyPress();
+				}
+			} catch(IllegalStateException e) {
+				// Sometimes this exception occurs from A*. Not sure why, but we can take this to mean the level has a problem and deserves bad fitness.
 			}
 		}
-
-		return new Pair<double[], double[]>(new double[]{distanceToTriforce, -numRooms}, new double[] {numRooms, numRoomsTraversed, searchStatesVisited});
+		return new Pair<double[], double[]>(new double[]{distanceToTriforce, -numRooms, numRooms == 0 ? 0 : (numRoomsTraversed*1.0)/numRooms}, new double[] {numRooms, numRoomsTraversed, searchStatesVisited});
 	}
 }
