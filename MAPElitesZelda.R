aadb92666ff1d7f0f589602e91bfaade96e71e42
Jacob Schrum <schrum2@southwestern.edu>
2020-01-25 00:29:06 -0600

    Final R Script for MAP Elites heat map
    Run from command line with name of log file and it will make a pdf in the same directory

--- MAPElitesZelda.R
+++ MAPElitesZelda.R
@@ -1,5 +1,12 @@
-setwd("E:\\Users\\he_de\\workspace\\GameGAN")
-map <- read.table("zeldacppntogan/MAPElites3/ZeldaCPPNtoGAN-MAPElites3_MAPElites_log.txt")
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  stop("Supply file name of MAP Elites log for Zelda", call.=FALSE)
+}
+
+print("Load data")
+#map <- read.table("zeldacppntogan/MAPElites3/ZeldaCPPNtoGAN-MAPElites3_MAPElites_log.txt")
+map <- read.table(args[1])
 # Only the final archive matters
 lastRow <- map[map$V1 == nrow(map) - 1, ]
 archive <- data.frame(matrix(unlist(lastRow[2:length(lastRow)]), nrow=(length(lastRow)-1), byrow=T))
@@ -7,6 +14,8 @@ names(archive) <- "PercentTraversed"
 
 # Add data indicating how the data is binned, based on convention of how
 # output data is organized
+
+print("Organize bins")
 
 wallBin <- append(rep(0, 10*101), rep(1, 10*101))
 wallBin <- append(wallBin, rep(2, 10*101))
@@ -43,11 +52,15 @@ allData <- data.frame(archive, wallBin, waterBin, roomBin)
 library(ggplot2)
 library(dplyr)
 library(viridis)
+library(stringr)
 
 # Bin for dungeons with 0 rooms doesn't actually have anything
 dropRooms0 <- filter(allData, roomBin > 0)
 
-pdf("FinalMAPArchive.pdf")  
+print("Create plot and save to file")
+
+outputFile <- str_replace(args[1],"txt","heat.pdf")
+pdf(outputFile)  
 result <- ggplot(dropRooms0, aes(x=waterBin, y=wallBin, fill=PercentTraversed)) +
   geom_tile() +
   facet_wrap(~roomBin) +
@@ -70,3 +83,6 @@ result <- ggplot(dropRooms0, aes(x=waterBin, y=wallBin, fill=PercentTraversed)) 
         axis.text = element_blank())
 print(result)
 dev.off()
+
+print(paste("Saved:",outputFile))
+print("Finished")
