48701ee05e3d241af69d3ea13662bc9886a0080c
schrum2 <schrum2@southwestern.edu>
2020-02-02 20:34:41 -0600

    Don't setwd

--- AveragesWithErrorBars.R
+++ AveragesWithErrorBars.R
@@ -4,7 +4,7 @@ if (length(args)==0) {
   stop("Supply Zelda or Mario", call.=FALSE)
 }
 
-setwd("E:\\Users\\he_de\\workspace\\GameGAN")
+#setwd("E:\\Users\\he_de\\workspace\\GameGAN")
 
 game <- args[1]
 #game <- "Zelda"
