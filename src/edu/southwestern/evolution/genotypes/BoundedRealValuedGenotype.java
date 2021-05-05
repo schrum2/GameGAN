b44f61aba981e568612751bd6b007a5cca880ac4
Jacob Schrum <schrum2@southwestern.edu>
2021-05-05 16:39:23 -0500

    Genotype updates
    These were requires to support the CPPN to Direct conversion (though there are also some extra comments)

--- src/edu/southwestern/evolution/genotypes/BoundedRealValuedGenotype.java
+++ src/edu/southwestern/evolution/genotypes/BoundedRealValuedGenotype.java
@@ -19,23 +19,36 @@ public class BoundedRealValuedGenotype extends RealValuedGenotype {
 
 	double[] lower; // Lowest allowable value for each gene position
 	double[] upper; // Highest allowable value for each gene position
-	boolean polynomialMutation; // Whether or not polynomial mutation should be
-								// used
+	boolean polynomialMutation; // Whether or not polynomial mutation should be used
 
+	/**
+	 * Creates evolvable genotype where genes are restricted to the default MMNEAT upper and lower bounds
+	 * Populates the genotype with values between the default MMNEAT upper and lower bounds
+	 */
 	public BoundedRealValuedGenotype() {
-		// May need to change this if other tasks start using the real-coded
-		// genotype
+		// May need to change this if other tasks start using the real-coded genotype
 		this(MMNEAT.getLowerBounds(), MMNEAT.getUpperBounds());
 	}
 
+	/**
+	 * Creates evolvable genotype where genes are restricted to certain bounds
+	 * Populates the genotype with values between certain bounds
+	 * @param lower Array of lower bounds for each gene position
+	 * @param upper Array of upper bounds for each gene position
+	 */
 	public BoundedRealValuedGenotype(double[] lower, double[] upper) {
 		this(RandomNumbers.randomBoundedArray(lower, upper), lower, upper);
 	}
 
-	private BoundedRealValuedGenotype(double[] genes, double[] lower, double[] upper) {
+	/**
+	 * Creates evolvable genotype where genes are restricted to certain bounds
+	 * @param genes Array of doubles corresponding to starting gene values
+	 * @param lower Array of lower bounds for each gene position
+	 * @param upper Array of upper bounds for each gene position
+	 */
+	public BoundedRealValuedGenotype(double[] genes, double[] lower, double[] upper) {
 		super(genes);
-		// Specialized mutation operator slightly more complicated than simple
-		// perturbation
+		// Specialized mutation operator slightly more complicated than simple perturbation
 		polynomialMutation = Parameters.parameters.booleanParameter("polynomialMutation");
 
 		this.lower = lower;
@@ -43,15 +56,30 @@ public class BoundedRealValuedGenotype extends RealValuedGenotype {
 		bound();
 	}
 
+	/**
+	 * Creates evolvable genotype where genes are restricted to certain bounds
+	 * @param genes ArrayList of doubles corresponding to starting gene values
+	 * @param lower Array of lower bounds for each gene position
+	 * @param upper Array of upper bounds for each gene position
+	 */
 	public BoundedRealValuedGenotype(ArrayList<Double> genes, double[] lower, double[] upper) {
 		this(ArrayUtil.doubleArrayFromList(genes), lower, upper);
 	}
 
+	/**
+	 * Creates evolvable genotype where genes are restricted to certain bounds
+	 * @param genotype RealValuedGenotype to provide starting the genotype
+	 * @param lower Array of lower bounds for each gene position
+	 * @param upper Array of upper bounds for each gene position
+	 */
 	@SuppressWarnings("unused")
 	private BoundedRealValuedGenotype(RealValuedGenotype genotype, double[] lower, double[] upper) {
 		this(genotype.genes, lower, upper);
 	}
 
+	/**
+	 * Returns a copy of genotype
+	 */
 	@Override
 	public Genotype<ArrayList<Double>> copy() {
 		double[] array = new double[genes.size()];
@@ -61,6 +89,10 @@ public class BoundedRealValuedGenotype extends RealValuedGenotype {
 		return new BoundedRealValuedGenotype(array, lower, upper);
 	}
 
+	/**
+	 * Finds and returns the range between the upper and lower bounds of each gene
+	 * @return An array of doubles corresponding to the range of each gene
+	 */
 	public final double[] getRange() {
 		double[] magnitudes = new double[lower.length];
 		for (int i = 0; i < magnitudes.length; i++) {
@@ -69,21 +101,22 @@ public class BoundedRealValuedGenotype extends RealValuedGenotype {
 		return magnitudes;
 	}
 
+	/**
+	 * Mutates the genotype
+	 */
 	@Override
 	public void mutate() {
-		if (polynomialMutation) { // Specialized mutation operator slightly more
-									// complicated than simple perturbation
+		if (polynomialMutation) { // Specialized mutation operator slightly more complicated than simple perturbation
 			new PolynomialMutation().mutate(this);
 		} else { // Default
 			new PerturbMutation(getRange()).mutate(this);
 		}
-
 		bound();
 	}
 
 	/**
-	 * Push gene values that are out of bounds back to the particular bound they
-	 * crossed.
+	 * Push gene values that are out of bounds back 
+	 * to the particular bound they crossed.
 	 */
 	public final void bound() {
 		for (int i = 0; i < genes.size(); i++) {
@@ -97,15 +130,25 @@ public class BoundedRealValuedGenotype extends RealValuedGenotype {
 		}
 	}
 
+	/**
+	 * Gets a new instance of a BoundedRealValuedGenotype with the same bounds
+	 * @return Returns a new instance of a BoundedRealValuedGenotype with the same bounds
+	 */
 	@Override
 	public Genotype<ArrayList<Double>> newInstance() {
 		return new BoundedRealValuedGenotype(lower, upper);
 	}
 
+	/**
+	 * Returns an array of doubles corresponding to the lower bounds of each gene
+	 */
 	public double[] lowerBounds() {
 		return lower;
 	}
 
+	/**
+	 * Returns an array of doubles corresponding to the upper bounds of each gene
+	 */
 	public double[] upperBounds() {
 		return upper;
 	}
--- src/edu/southwestern/evolution/genotypes/NumericArrayGenotype.java
+++ src/edu/southwestern/evolution/genotypes/NumericArrayGenotype.java
@@ -15,14 +15,25 @@ import java.util.ArrayList;
  */
 public abstract class NumericArrayGenotype<T> implements Genotype<ArrayList<T>> {
 
+	@SuppressWarnings("rawtypes")
+	private static final ArrayCrossover ARRAY_CROSSOVER = new ArrayCrossover();
+	
 	ArrayList<T> genes;
 	private long id = EvolutionaryHistory.nextGenotypeId();
-
+	
+	/**
+	 * Creates an evolvable genotype that is an arraylist of numbers
+	 * @param genes An arraylist of numbers to initialize the genotype
+	 */
 	@SuppressWarnings("unchecked")
 	public NumericArrayGenotype(ArrayList<T> genes) {
 		this.genes = (ArrayList<T>) genes.clone();
 	}
 
+	/**
+	 * Creates an evolvable genotype that is an arraylist of numbers
+	 * @param genes An array of numbers to initialize the genotype
+	 */
 	public NumericArrayGenotype(T[] genes) {
 		this.genes = new ArrayList<T>(genes.length);
 		for (int i = 0; i < genes.length; i++) {
@@ -30,27 +41,50 @@ public abstract class NumericArrayGenotype<T> implements Genotype<ArrayList<T>> 
 		}
 	}
 
+	/**
+	 * Sets the value of a specific gene in the genotype
+	 * @param pos An integer specifying the gene's location
+	 * @param value A number to set the gene's value to
+	 */
 	public void setValue(int pos, T value) {
 		genes.set(pos, value);
 	}
 
+	/**
+	 * Crossover this genotype with another
+	 * @param g Another genotype to crossover with
+	 * @return The resulting genotype post-crossover
+	 */
+	@SuppressWarnings("unchecked")
 	public Genotype<ArrayList<T>> crossover(Genotype<ArrayList<T>> g) {
-		return new ArrayCrossover<T>().crossover(this, g);
+		return ARRAY_CROSSOVER.crossover(this, g);
 	}
 
+	/**
+	 * Returns an arraylist of the gene values
+	 */
 	public ArrayList<T> getPhenotype() {
 		return genes;
 	}
-
+	
+	/**
+	 * Returns the genotype id and genes as a string
+	 */
 	@Override
 	public String toString() {
 		return getId() + ":" + genes.toString();
 	}
 
+	/**
+	 * Returns the genotype id
+	 */
 	public long getId() {
 		return id;
 	}
 	
+	/**
+	 * Returns an arraylist of the genes
+	 */
 	public ArrayList<T> getGenes() {
 		return genes;
 	}
